b8f4fec592c4d40024829e720b057b817fbdf860
Fernanda D. da Costa <ffernandaa92@gmail.com>
2022-05-27 02:29:33 -0300

    Criando endpoints psicologos

--- la_vie_api/src/controllers/psicologoController.js
+++ la_vie_api/src/controllers/psicologoController.js
@@ -1,4 +1,71 @@
 const { Psicologo } = require("../models/");
 
+const atributosRetorno = ['id', 'nome', 'email', 'apresentacao'];
+
+const PsicologoController = {
+    
+    index: async (req, res) => {
+        const dados = await Psicologo.findAll({attributes: atributosRetorno});
+        res.json(dados);
+    },
+
+    show: async (req, res) => {
+        const { id } = req.params;
+        const dado = await Psicologo.findByPk(id, {attributes: atributosRetorno});
+        if(!dado){
+            res.status(404).json({
+                message: "Psicologo não encontrado"
+            });
+            return;
+        }
+
+        res.json(dado);
+    },
+
+    store: async (req, res) => {
+        const dados = await Psicologo.create(req.body);
+        res.status(201).json(dados);
+    },
+
+    update: async (req, res) => {
+        const { id } = req.params;
+        const dados = req.body;
+        const paciente = await Psicologo.findByPk(id, {attributes: atributosRetorno});
+        
+        if(!paciente){
+            res.status(404).json({
+                message: "Psicologo não encontrado"
+            });
+            return;
+        }
+
+        for(let atributo in dados){
+            paciente[atributo] = dados[atributo];
+        }
+
+        await paciente.save();
+
+        const pacienteUpdate = await Psicologo.findByPk(id, {attributes: atributosRetorno});
+
+        res.status(200).json(pacienteUpdate);
+    },
+
+    destroy: async (req, res) => {
+        const { id } = req.params;
+        const paciente = await Psicologo.findByPk(id);
+        
+        if(!paciente){
+            res.status(404).json({
+                message: "Psicologo não encontrado"
+            });
+            return;
+        }
+
+        await paciente.destroy();
+
+        res.status(204).send("Psicologo excluido");
+    },
+
+}
 
 module.exports = PsicologoController;
--- la_vie_api/src/routes/index.js
+++ la_vie_api/src/routes/index.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 
 // const atendimentoController = require("../controllers/atendimentoController");
 const pacienteController = require("../controllers/pacienteController");
-// const psicologosController = require("../controllers/psicologoController");
+const psicologoController = require("../controllers/psicologoController");
 const authController = require("../controllers/authController");
 const dashboardController = require("../controllers/dashboardController");
 
@@ -19,5 +19,12 @@ routes.get('/pacientes/:id', pacienteController.show);
 routes.post('/pacientes', pacienteController.store);
 routes.put('/pacientes/:id', pacienteController.update);
 routes.delete('/pacientes/:id', pacienteController.destroy);
+
+routes.get('/psicologos', psicologoController.index);
+routes.get('/psicologos/:id', psicologoController.show);
+routes.post('/psicologos', psicologoController.store);
+routes.put('/psicologos/:id', psicologoController.update);
+routes.delete('/psicologos/:id', psicologoController.destroy);
+
 
 module.exports = routes;
